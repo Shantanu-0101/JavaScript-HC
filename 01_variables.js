2d5cc1ae45adbbb454fd8a28274cf26bfc94ad9b
Shantanu Panchal <spanchal112000@gmail.com>
2025-09-21 18:34:16 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 143254
+let accountEmail = "Shant@gmail.com"
+var accountPassword = "1234"
+accountCity = "Aurangabad"
+
+
+accountEmail = "SP@Google.com"
+accountPassword = "123213"
+accountCity = "Mumbai"
+
+console.log(accountID)
+
+console.table([accountID, accountEmail,accountPassword,accountCity])
