61cb741b53b5c5e9ebe8bf9b47b32d4d902f2346
Shantanu Panchal <spanchal112000@gmail.com>
2025-09-23 09:14:01 +0530

    Add files via upload

---
+++ 02_dtypes.js
@@ -0,0 +1,10 @@
+let accountNo = 232332;
+let accountName = 'Shantanu Panchal'
+let ROI = 8.9;
+let accountVal = true;
+let accountStatus;
+let accountCity = null;
+
+console.table([accountNo, accountName, ROI, accountVal, accountStatus, accountCity])
+
+console.table([typeof accountName, typeof accountNo, typeof ROI, typeof accountVal, typeof accountStatus, typeof accountCity ])
