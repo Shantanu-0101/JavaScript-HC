0b5a9d4fdfdce48e330d499d12e0cb23abbbbf3c
Shantanu Panchal <spanchal112000@gmail.com>
2025-09-25 17:54:24 +0530

    Add files via upload

---
+++ 03_Dtype_conversion.js
@@ -0,0 +1,19 @@
+let age = "29abc" // It is string now
+console.log(typeof age)
+
+let numericAge = Number(age) // converted into numeric form
+console.log(numericAge)
+console.log(typeof numericAge)
+
+let myName = 29;
+console.log(typeof myName)
+
+let strName = String(myName)
+console.log(typeof strName)
+
+let mySts = "TRUE"
+console.log(typeof mySts)
+
+let chkmySts = Boolean(mySts)
+console.log(chkmySts)
+console.log(typeof chkmySts)
