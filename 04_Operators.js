679854ca5fddf5a19066fc4b08fb57d5ee1b9036
Shantanu Panchal <spanchal112000@gmail.com>
2025-09-26 19:48:40 +0530

    Add files via upload

---
+++ 04_Operators.js
@@ -0,0 +1,58 @@
+/*
+Arithematic Operators
+Assignment Operators
+Comparison Operators
+String Operators
+Logical Operators
+Bitwise Operators
+Ternary Operators
+*/
+
+// let num1 = 5
+// let num2 = 7
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+
+// console.log("------------------------")
+
+// let x = 5
+// let y = 2
+
+// console.log(x ** y)
+
+// result = x + y
+// console.log(result)
+
+// let x = 5
+// let y = x
+
+// console.log(y)
+
+// x += 3
+// console.log(x)
+
+
+// Comparison Operator
+// <, >, <=, >=, !=, ==, ===, !==
+
+let age = 21
+console.log(age = 21)
+
+console.log(age > 18)
+
+
+let s = 'A'
+let b = 2
+
+console.log(s < b)
+
+let num = '5'
+let numbe = 5
+
+console.log(num == numbe)
+
+console.log(num === numbe)
