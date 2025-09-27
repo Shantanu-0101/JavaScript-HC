3061f70096cc00250d1174780b014f206e2f213d
Shantanu Panchal <spanchal112000@gmail.com>
2025-09-27 20:17:23 +0530

    if-else

---
+++ 05_Control_Statements.js
@@ -0,0 +1,89 @@
+// control statements
+//  if and else
+
+// let age = 12
+
+// if(age >=18)
+// {
+// console.log("Allowed")
+// }
+// else
+// {
+//     console.log("Not Allowed")
+// }
+
+// let x = 5
+// let y = 6
+// let z = 7
+
+// if(x>y && x>z)
+// {
+//  console.log("X is greater")
+// }
+// else if(y>x && y>z)
+// {
+//  crossOriginIsolated.log("Y is greater")
+// }
+// else
+// {
+//     console.log("Z is greater")
+// }
+
+// let num = 45
+
+// if(num % 2 == 0)
+// {
+//     console.log("Number is even")
+// }
+// else
+// {
+//     console.log("Number is odd")
+// }
+
+
+// Switch Case --> Multiple cases
+
+// let num = 7
+// switch(num)
+// {
+//     case 1 : console.log("One");
+//         break;
+//     case 2 : console.log("Two");
+//         break;
+//     case 3 : console.log("Three");
+//         break;
+//     default: console.log("wrong choice")
+
+// }
+
+
+
+// let choice = 'c'
+// switch(choice)
+// {
+//     case 'a' : console.log("One");
+//         break;
+//     case 'b' : console.log("Two");
+//         break;
+//     case 'c' : console.log("Three");
+//         break;
+//     default: console.log("wrong choice")
+
+// }
+
+
+let choice = 'one'
+switch(choice)
+{
+    case 'one' : console.log("One");
+        break;
+    case 'b' : console.log("Two");
+        break;
+    case 'c' : console.log("Three");
+        break;
+    default: console.log("wrong choice")
+
+}
+
+
+
