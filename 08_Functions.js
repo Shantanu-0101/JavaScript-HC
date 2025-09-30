3c130bbd12a5d78e6e76b8564036717293e72569
Shantanu Panchal <spanchal112000@gmail.com>
2025-09-30 20:11:46 +0530

    Add files via upload

---
+++ 08_Functions.js
@@ -0,0 +1,66 @@
+// function shant()
+// {
+//     console.log("Welcome everyone")
+// }
+
+
+// function yogesh()
+// {
+//     console.log("Nice to meet you")
+// }
+
+// shant() 
+// yogesh()
+
+//----------------------------------------
+
+// function addition(num1, num2)
+// {
+//  result = num1 + num2
+//  return result
+
+// }
+
+// let res = addition(5,5)
+// console.log(res )
+
+//-------------------------------------------
+
+// display = function(){
+//     console.log("Hello Everyone")
+// }
+
+// display()
+
+//-------------------------------------------
+
+// display = () => {
+//     console.log("Hello World")
+// }
+
+// display();
+
+//------------------------------------------
+
+// display = (num1, num2) => {
+//     console.log(`Addition is ${num1 + num2}`)
+// }
+
+// display(2,2)
+
+//------------------------------------------
+
+
+// display = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// let res = display(2,2)
+// console.log(res)
+
+//------------------------------------------
+
+display = (num1, num2) =>  num1 + num2
+
+let result = display(5,5)
+console.log(result)
