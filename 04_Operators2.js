246fdf8f79407f3c47762bd996486c2c35a6fb19
Shantanu Panchal <spanchal112000@gmail.com>
2025-09-30 20:11:20 +0530

    Add files via upload

---
+++ 04_Operators2.js
@@ -0,0 +1,76 @@
+// String Operators
+
+
+// let x = 'Ramesh'
+// let y = 'Ajay'
+// let percentage = 70
+
+// console.log(x + " " + y + " " +percentage)
+
+
+// console.log(`${x} ${y} ${percentage}`)
+
+// console.log(`Student-1 ${x} Student-2:${y} Percentage:${percentage}`)
+
+
+// Logical Opearators
+
+// let age = 18
+// let gen = 'Male'
+
+//COndition age >=18 gender = male
+
+// console.log(age >= 18 && gen == 'Male')
+
+
+// let age1 = 12
+// let gen1 = 'Male'
+
+//COndition age >=18 gender = male
+
+// console.log(age >= 18 && gen == 'Male')
+
+
+// let age2 = 18
+// let gen2 = 'Male'
+
+//COndition age >=18 gender = male
+
+// console.log(age >= 18 ||  gen == 'Male')
+
+
+// Bitwise Opearator
+
+/* &
+
+1 1 => 1
+otherwise 0
+
+
+|
+0 0 => 0
+otherwise 1
+
+*/
+
+// let x = 7 // Convert into binary => 0111
+// let y = 15 // Convert into binary => 1111
+
+// console.log(x & y)
+
+// console.log(x | y)
+
+// Shift Operators
+
+// console.log(x >> 2)
+// console.log(y >> 2)
+// console.log(x << 2)
+// console.log(y << 2)
+
+
+// Ternary Operator
+
+let x = 15
+let y = 7
+
+console.log(x > y ? "X is greater" : "Y is greater")
