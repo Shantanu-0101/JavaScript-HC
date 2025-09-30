3a88fd3365099e6e9603b41f1208cec75ee962db
Shantanu Panchal <spanchal112000@gmail.com>
2025-09-30 20:10:36 +0530

    Add files via upload

---
+++ 07_Array.js
@@ -0,0 +1,23 @@
+ let myArr = [1,2,3,4, "Ajay"]
+
+// console.log(myArr)
+// console.log(typeof myArr)
+// console.log( myArr.length)
+
+// console.log(myArr[1])
+// console.log(myArr.at(1))
+
+// myArr[2] = 44
+// console.log(myArr)
+
+// console.log(myArr)
+// myArr.push(99)
+// console.log(myArr)
+
+
+// let x = myArr.pop
+// console.log(myArr.length)
+
+console.log(myArr)
+delete myArr[2]
+console.log(myArr)
