6c83f98338a604e07becebdea659fb93c7b87bab
Shantanu Panchal <spanchal112000@gmail.com>
2025-10-01 19:59:32 +0530

    Add files via upload

---
+++ 11_map_functions.js
@@ -0,0 +1,39 @@
+// const mylist = [11,22,33,44]
+
+// const newList = mylist.map(myFunction)
+
+// function myFunction(data){
+//     return data 
+// }
+
+// console.log(newList)
+
+// const mylist = [11,22,33,44]
+
+// const newList = mylist.map( (data) => 
+// {
+//     return data 
+// })
+
+// console.log(newList)
+
+
+
+// const mylist = [11,22,33,44]
+
+// const newList = mylist.filter( (data) => 
+// {
+//     return data 
+// })
+
+// console.log(newList)
+
+
+const mylist = [11,22,33,44]
+
+const newList = mylist.every( (data) => 
+{
+    return data < 50
+})
+
+console.log(newList)
---
+++ 12_callback.js
@@ -0,0 +1,61 @@
+// const { useCallback } = require("react")
+
+// function display( result ){
+//     console.log( result )
+// }
+
+// function add(x,y, callBack){
+//     let res = x + y
+//     callBack( res )
+// }
+
+// function multiplication(x,y){
+//     let res = x * y
+//     display( res )
+// }
+
+// multiplication(5,22)
+// add(5,5)
+
+// function multiplication(x,y, callBack){
+//     let res = x * y
+//     callBack( res )
+// }
+
+// multiplication(5,2, callBack)
+// add(5,5) 
+
+
+
+// setTimeout(display, 3000)
+
+// function display()
+// {
+//     console.log("Hello World")
+// }
+
+
+// console.log("Starting line");
+
+// setTimeout(display, 3000)
+
+// function display()
+// {
+//     console.log("Hello World")
+// }
+
+// console.log("ending line");
+
+
+
+console.log("Starting line");
+
+setInterval(display, 3000)
+
+function display()
+{
+    console.log("Hello World")
+}
+
+console.log("ending line");
+
